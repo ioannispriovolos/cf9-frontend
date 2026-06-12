77150d3b4149f46ce6ffec29ab8af141053b3e38
ioannispriovolos <priovolosg@hotmail.com>
2026-06-12 11:06:45 +0300

    feat: add navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,13 @@ import { Step3PersonTableInput } from './components/step3-person-table-input/ste
 import { IPerson } from './shared/interfaces/person';
 import { Step4ForDirective } from './components/step4-for-directive/step4-for-directive';
 import { Step5EventBind } from './components/step5-event-bind/step5-event-bind';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
+    Navbar,
     Step2PersonTable, 
     Step3PersonTableInput, 
     Step4ForDirective,
