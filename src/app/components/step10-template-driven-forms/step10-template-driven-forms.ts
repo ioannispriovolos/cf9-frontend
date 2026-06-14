dbac06ae518131c1abd50d61129ac029b3eab06b
ioannispriovolos <priovolosg@hotmail.com>
2026-06-14 22:33:20 +0300

    feat: add template forms

--- src/app/components/step10-template-driven-forms/step10-template-driven-forms.ts
+++ src/app/components/step10-template-driven-forms/step10-template-driven-forms.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { PersonTemplateForm } from '../step10-template-driven-forms/person-template-form/person-template-form';
+import { Step3PersonTableInput } from '../step3-person-table-input/step3-person-table-input';
+import { Step8SimpleDataTable } from '../step8-simple-data-table/step8-simple-data-table';
+import { IPerson } from '../../shared/interfaces/person';
 
 @Component({
   selector: 'app-step10-template-driven-forms',
   imports: [
-    PersonTemplateForm
-  ],
+    PersonTemplateForm,
+    Step3PersonTableInput,
+    Step8SimpleDataTable
+],
   templateUrl: './step10-template-driven-forms.html',
   styleUrl: './step10-template-driven-forms.css',
 })
-export class Step10TemplateDrivenForms {}
+export class Step10TemplateDrivenForms {
+  currentPerson: IPerson | undefined;
+  persons: IPerson[] = [];
+
+  onPerson(data: IPerson) {
+    this.currentPerson = data;
+    this.persons = [...this.persons, data];
+  }
+}
