e64e4037ae41eadfc8572bb5909e328e14946d98
ioannispriovolos <priovolosg@hotmail.com>
2026-06-14 19:42:07 +0300

    feat: add template driven form

---
+++ src/app/components/step10-template-driven-forms/step10-template-driven-forms.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PersonTemplateForm } from '../step10-template-driven-forms/person-template-form/person-template-form';
+
+@Component({
+  selector: 'app-step10-template-driven-forms',
+  imports: [
+    PersonTemplateForm
+  ],
+  templateUrl: './step10-template-driven-forms.html',
+  styleUrl: './step10-template-driven-forms.css',
+})
+export class Step10TemplateDrivenForms {}
--- src/app/components/step7-list-group-menu/step7-list-group-menu.ts
+++ src/app/components/step7-list-group-menu/step7-list-group-menu.ts
@@ -17,6 +17,7 @@ export class Step7ListGroupMenu {
     { text: "Step 4 @for Directive", appLink: "for-directive-example" },
     { text: "Step 5 Event Bind", appLink: "event-bind-example" },
     { text: "Step 8 Simple Data Table", appLink: "simple-datatable-example" },
-    { text: "Step 9 Output Example", appLink: "component-output-example" }
+    { text: "Step 9 Output Example", appLink: "component-output-example" },
+    { text: "Step 10 Template Forms", appLink: "template-driven-form-example" }
   ]
 }
