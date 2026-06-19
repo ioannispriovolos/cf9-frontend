7df5382e1a0387a203e4994a5246e15dc2231dd4
ioannispriovolos <priovolosg@hotmail.com>
2026-06-19 19:46:55 +0300

    feat: add guards

--- src/app/components/step13-user-login/step13-user-login.ts
+++ src/app/components/step13-user-login/step13-user-login.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { jwtDecode } from 'jwt-decode';
 import { Credentials, LoggedInUser } from '../../shared/interfaces/user-login.interface';
 import { UserService } from '../../shared/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-step13-user-login',
@@ -14,6 +15,9 @@ import { UserService } from '../../shared/services/user.service';
 })
 export class Step13UserLogin {
   userService = inject(UserService);
+  router = inject(Router);
+
+  user = this.userService.user;
 
   form = new FormGroup({
     username: new FormControl('', [Validators.required]),
@@ -33,6 +37,7 @@ export class Step13UserLogin {
           email: decodedToken.email,
           roles: decodedToken.roles
         });
+        this.router.navigate(['restricted-content']);
       },
       error: (error) => {
         console.log(error);
---
+++ src/app/shared/guards/reader-role-guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { IRole } from '../interfaces/user-login.interface';
+
+export const readerRoleGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  const userRoles: IRole[] | undefined = userService.user()?.roles;
+  const hasPermission = userRoles?.some((r: IRole) => { r.role === 'READER' && r.active});
+
+  if (userService.user() && hasPermission) {
+    return true;
+  }
+
+  return router.navigate(['user-login']);
+};
