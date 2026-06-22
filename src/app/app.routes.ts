2eccd4fecb89b9b6c5f2a082b6679cf0702523fb
ioannispriovolos <priovolosg@hotmail.com>
2026-06-22 11:58:11 +0300

    feat: add user registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { Step13UserLogin } from './components/step13-user-login/step13-user-logi
 import { RestrictedContent } from './components/restricted-content/restricted-content';
 import { authGuard } from './shared/guards/auth-guard';
 import { readerRoleGuard } from './shared/guards/reader-role-guard';
+import { Step14UserRegistration } from './components/step14-user-registration/step14-user-registration';
 
 export const routes: Routes = [
     {path: 'person-table-example', component: Step2PersonTable},
@@ -27,6 +28,11 @@ export const routes: Routes = [
     {path: 'reactive-form-example', component: Step11ReactiveForms},
     {path: 'http-client-example', component: Step12HttpClientService},
     {path: 'user-login', component: Step13UserLogin},
+    {
+        path: 'user-registration-example', 
+        component: Step14UserRegistration,
+        canActivate: [authGuard, readerRoleGuard]
+    },
     {
         path: 'restricted-content', 
         component: RestrictedContent,
---
+++ src/app/components/step14-user-registration/step14-user-registration.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step14-user-registration',
+  imports: [],
+  templateUrl: './step14-user-registration.html',
+  styleUrl: './step14-user-registration.css',
+})
+export class Step14UserRegistration {}
